ac802eb98310b61e544c3b33212098b66aa66296
Zolottsev <sky.pnz@mail.ru>
2024-06-10 22:42:35 +0300

    HelloWorlTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-public class HelloWorlTest {
+public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
         System.out.println("Hello world!");
