aeb0caf2ef9aac9754c144e82641c3dca649c290
Zolottsev <sky.pnz@mail.ru>
2024-06-30 13:56:09 +0300

    Ex11GetCookie

--- src/test/java/tests/Ex11GetCookie.java
+++ src/test/java/tests/Ex11GetCookie.java
@@ -2,25 +2,20 @@ package tests;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import java.util.HashMap;
+
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class GetCookie {
+public class Ex11GetCookie {
     @Test
     public void testGetCookie() {
-        Map<String, String> authData = new HashMap<>();
-        authData.put("email", "vinkotov@examle.com");
-        authData.put("password", "1234");
-
-        Response responseGetCookie = RestAssured
-                .given()
-                .body(authData)
+            Response responseGetCookie = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_cookie")
                 .andReturn();
 
         Map<String,String> cookies = responseGetCookie.getCookies();
         System.out.println(cookies.toString());
+
         assertTrue(cookies.containsKey("HomeWork"), "Response doesn't have 'HomeWork' cookie");
         assertTrue(cookies.containsValue("hw_value"), "Response doesn't have cookie with value 'hw_value'");
     }
